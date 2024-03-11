5e6c975260873a8122d5cb6549323dfffb3b8d53
kamenari <kamenaris@gmail.com>
2024-03-11 21:56:37 +0900

    不要なコードを削除

--- client/src/app/bpmn-js/page.tsx
+++ client/src/app/bpmn-js/page.tsx
@@ -2,14 +2,13 @@
 import BpmnModelerComponents from '@/components/BpmnModelerComponents';
 import ModelerComponent from '@/components/ModelerComponents';
 // BPMN図のXMLデータをインポート
-import diagramXML from '@/components/diagram';
 
 const bpmnJs: React.FC = () => {
 
   return (
     // diagramXMLはBPMN図のXMLデータを含んでおり、このコンポーネントに渡されます。
     // <BpmnModelerComponents xml={diagramXML} />
-    <ModelerComponent url={diagramXML} />
+    <ModelerComponent />
   );
 };
 
